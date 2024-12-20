1acb7098247c8ba104a5e5bd7970a3513c12d1de
FarukErat <farukerat1327@gmail.com>
2024-12-20 22:29:01 +0300

    format code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById("surprise-btn").addEventListener("click", () => {
-    const surprise = document.getElementById("surprise");
-    if (surprise.classList.contains("hidden")) {
-      surprise.classList.remove("hidden");
-    } else {
-      surprise.classList.add("hidden");
-    }
-  });
+  const surprise = document.getElementById("surprise");
+  if (surprise.classList.contains("hidden")) {
+    surprise.classList.remove("hidden");
+  } else {
+    surprise.classList.add("hidden");
+  }
+});
